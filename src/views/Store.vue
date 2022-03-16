6f12fa49574007b75febf725910448a0930f8be7
DESKTOP-C6KU3VI\f <dlrb1212@gmail.com>
2022-03-16 19:38:23 +0900

    매장관리 퍼블

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -16,15 +16,19 @@
             height="150px"
             src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
           > -->
-          <v-card-title>{{ item.number }}번 테이블</v-card-title>
+          <v-card-title class="c-title">
+            <tr>
+              <td>{{ item.number }}번 테이블</td>
+              <td>{{ item.totalPrice }}원</td>
+            </tr>
+            <v-card-subtitle class="pb-0">
+              {{ item.time }} 입장
+            </v-card-subtitle>
+          </v-card-title>
 
           <!-- </v-img> -->
 
-          <v-card-subtitle class="pb-0">
-            입장시간 - {{ item.time }}
-          </v-card-subtitle>
-
-          <v-card-text class="text--primary">
+          <v-card-text class="text--primary c-text">
             <tr
               v-for="(menu, index) in item.menu"
               :key="index"
@@ -51,18 +55,20 @@ export default {
     items: [
       {
         number: "1",
+        totalPrice: 91000,
         time: "11:20",
         menu: [
           { name: "닭도리탕", price: 24000, ea: 1 },
           { name: "제육볶음", price: 8000, ea: 3 },
           { name: "계란찜", price: 5000, ea: 2 },
           { name: "라면", price: 3000, ea: 2 },
-          { name: "참이슬", price: 4000, ea: 13 },
+          { name: "참이슬", price: 4000, ea: 3 },
           { name: "카스", price: 5000, ea: 3 }
         ]
       },
       {
         number: "2",
+        totalPrice: 38000,
         time: "14:20",
         menu: [
           { name: "아구찜", price: 22000, ea: 1 },
@@ -74,6 +80,21 @@ export default {
 };
 </script>
 <style scoped>
+.c-title {
+  border-bottom: 2px solid gray;
+}
+.c-title tr {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+.v-card__title + .v-card__subtitle,
+.v-card__title + .v-card__text {
+  padding: 16px;
+}
+.v-card__subtitle {
+  padding: 0;
+}
 .menu-info {
   display: flex;
   width: 100%;
